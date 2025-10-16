56c4a434c29b8828e4bd9d308342e6604e771645
hamel-dev <bhamil179@gmail.com>
2025-10-16 12:13:53 +0000

    16/10/2025

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: '/api'
+  apiUrl: 'https://proxy-dev.cadorim.com/pharma-link/api'
 };
