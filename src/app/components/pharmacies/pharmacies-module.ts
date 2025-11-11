ac85b0d27d6308092623d1543c2fcb43b0766361
hamel-dev <bhamil179@gmail.com>
2025-11-11 21:56:12 +0000

    pharmacy management

---
+++ src/app/components/pharmacies/pharmacies-module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PharmaciesRoutingModule } from './pharmacies-routing-module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PharmaciesRoutingModule
+  ]
+})
+export class PharmaciesModule { }
