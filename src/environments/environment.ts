64314b76d8fc002b241e871ec7240415eba31bd9
hamel-dev <bhamil179@gmail.com>
2025-11-29 09:52:12 +0000

    29/11/2025

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,8 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: false,
-  //apiUrl: 'https://pharma-link-backend.onrender.com/api',
-  apiUrl: 'http://localhost:8080/api',
+  apiUrl: 'https://pharma-link-backend.onrender.com/api',
+  //apiUrl: 'http://localhost:8080/api',
   supabaseUrl: 'https://your-supabase-url.supabase.co',
   supabaseAnonKey: 'your-anon-key',
   supabaseServiceKey: 'your-service-role-key', // to be used server-side only
