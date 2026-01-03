59fd63dc848ca1537158d5dcd7fbf3b8867e8ff5
hamel-dev <bhamil179@gmail.com>
2026-01-03 12:14:30 +0000

    feat(environment): switch API URL for local development

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,8 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: false,
-  apiUrl: 'https://proxy-dev.cadorim.com/api-backend/api',
-  //apiUrl: 'http://localhost:8088/api',
+  //apiUrl: 'https://proxy-dev.cadorim.com/api-backend/api',
+  apiUrl: 'http://localhost:8088/api',
   supabaseUrl: 'https://xijniizcthuwwrmaqxtk.supabase.co',
   supabaseAnonKey:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhpam5paXpjdGh1d3dybWFxeHRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2MTkwODksImV4cCI6MjA3NDE5NTA4OX0.6_CP9xSExFZtP8DKpXL5-xnCOBLUAVhVP5CL9FEC8cY',
